ef45c79c9dc9265786831ca188ae7dd21b287b14
bugoao <46293847+bugoao@users.noreply.github.com>
2025-11-01 15:49:39 +0800

    Update TrackedEntity.entity.ts

--- src/ORM/utils/TrackedEntity.entity.ts
+++ src/ORM/utils/TrackedEntity.entity.ts
@@ -1,14 +1,13 @@
 import { CreateDateColumn, DeleteDateColumn, UpdateDateColumn } from 'typeorm';
-
 import { DateTimeTransformer } from './DateTimeTransformer';
 
 export abstract class TrackedEntity {
-    @DeleteDateColumn({ nullable: true, type: 'datetime' })
+    @DeleteDateColumn({ nullable: true, type: 'datetime', transformer: new DateTimeTransformer() })
     public deletedAt?: Date;
 
-    @CreateDateColumn({ type: 'datetime' })
-    public createdAt?: Date
+    @CreateDateColumn({ type: 'datetime', transformer: new DateTimeTransformer() })
+    public createdAt?: Date;
 
-    @UpdateDateColumn({ type: 'datetime' })
-    public updatedAt?: Date
+    @UpdateDateColumn({ type: 'datetime', transformer: new DateTimeTransformer() })
+    public updatedAt?: Date;
 }
