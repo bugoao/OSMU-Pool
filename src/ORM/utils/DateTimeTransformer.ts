a6c132e7dca2f57c00d47013069278ea82466bda
bugoao <46293847+bugoao@users.noreply.github.com>
2025-11-01 17:55:10 +0800

    Update DateTimeTransformer.ts

--- src/ORM/utils/DateTimeTransformer.ts
+++ src/ORM/utils/DateTimeTransformer.ts
@@ -2,12 +2,10 @@ import { ValueTransformer } from 'typeorm';
 
 export class DateTimeTransformer implements ValueTransformer {
     to(value: Date | null): Date | null {
-
         return value ?? null;
     }
 
     from(value: Date | null): Date | null {
-
         return value ?? null;
     }
 }
