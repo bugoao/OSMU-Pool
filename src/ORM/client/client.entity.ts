f785a272a1ef468d78c0f446f976ea59ed357352
bugoao <46293847+bugoao@users.noreply.github.com>
2025-11-01 17:18:05 +0800

    Update client.entity.ts

--- src/ORM/client/client.entity.ts
+++ src/ORM/client/client.entity.ts
@@ -1,18 +1,10 @@
 import { Column, Entity, Index, PrimaryColumn } from 'typeorm';
-
 import { DateTimeTransformer } from '../utils/DateTimeTransformer';
 import { TrackedEntity } from '../utils/TrackedEntity.entity';
 
-//https://www.sqlite.org/withoutrowid.html
-
-//The WITHOUT ROWID optimization is likely to be helpful for tables that have non-integer
-// or composite (multi-column) PRIMARY KEYs and that do not store large strings or BLOBs.
-//WITHOUT ROWID tables work best when individual rows are not too large.
 @Entity({ withoutRowid: true })
 @Index(['address', 'clientName', 'sessionId'], { unique: true })
 export class ClientEntity extends TrackedEntity {
-
-
     @PrimaryColumn({ length: 62, type: 'varchar' })
     address: string;
 
@@ -22,19 +14,15 @@ export class ClientEntity extends TrackedEntity {
     @PrimaryColumn({ length: 8, type: 'varchar' })
     sessionId: string;
 
-
     @Column({ length: 128, type: 'varchar', nullable: true })
     userAgent: string;
 
-
-
-    @Column({ type: 'datetime'})
+    @Column({ type: 'datetime', transformer: new DateTimeTransformer() })
     startTime: Date;
 
     @Column({ type: 'real', default: 0 })
-    bestDifficulty: number
+    bestDifficulty: number;
 
     @Column({ default: 0 })
     hashRate: number;
-
 }
