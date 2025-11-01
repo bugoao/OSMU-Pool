059e47e7159cdcb66909671f6998fcd107b83b41
bugoao <46293847+bugoao@users.noreply.github.com>
2025-11-01 18:29:04 +0800

    Update DateTimeTransformer.ts

--- src/ORM/utils/DateTimeTransformer.ts
+++ src/ORM/utils/DateTimeTransformer.ts
@@ -1,11 +1,12 @@
 import { ValueTransformer } from 'typeorm';
+import { format } from 'date-fns-tz';
 
 export class DateTimeTransformer implements ValueTransformer {
-    to(value: Date | null): Date | null {
-        return value ?? null;
-    }
+  to(value: Date | null): string | null {
+    return value ? format(value, 'yyyy-MM-dd HH:mm:ss', { timeZone: 'UTC' }) : null;
+  }
 
-    from(value: Date | null): Date | null {
-        return value ?? null;
-    }
+  from(value: string | null): Date | null {
+    return value ? new Date(value + 'Z') : null;
+  }
 }
